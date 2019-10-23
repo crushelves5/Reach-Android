8618ae7627e665e135e0eceaf0f81bfbf54cde32
Bethel Adaghe <betheladaghe@gmail.com>
2019-10-23 16:49:26 -0400

    added CreateActivity & ViewActivity added toolbar to mainActivity for navigation to other activities

--- app/src/main/java/com/example/reach/MainActivity.java
+++ app/src/main/java/com/example/reach/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.reach;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
 
 public class MainActivity extends AppCompatActivity {
-
+    final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "in onCreate()");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //get toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu m){
+        getMenuInflater().inflate(R.menu.menu_items, m);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem mi) {
+        int id = mi.getItemId();
+        switch (id){
+            case R.id.create:
+                Intent intent = new Intent(MainActivity.this,CreateActivity.class);
+                startActivity(intent);
+        }
+        return true;
     }
 }
